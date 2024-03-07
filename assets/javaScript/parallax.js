f5374bfa7033881fd7a51aa9f3de57b577dbdad6
Jonathan David <jhernadezcorrea@gmail.com>
2024-03-06 22:01:32 -0500

    working with the parallax

--- assets/javaScript/parallax.js
+++ assets/javaScript/parallax.js
@@ -1,10 +1,18 @@
 let rocket = document.getElementById('rocket');
 const initialTop = getComputedStyle(rocket).getPropertyValue('top');
-const initialTopInt = parseInt(initialTop, 10)
+const initialTopInt = parseInt(initialTop, 10);
+
+let moon = document.getElementById('moon-img');
+const initialwidth = getComputedStyle(moon).getPropertyValue('width');
+const initialwidthInt = parseInt(initialwidth,10)
+
+
 
 window.addEventListener('scroll', () => {
    let value = window.scrollY;
-   let valuew = rocket.style.top = (initialTopInt - value) + 'px'
+   rocket.style.top = (initialTopInt - value) + 'px'
+   moon.style.width = (initialwidthInt)*(1/((value/350)+1)) + 'px'
 
+  
 } )
 
